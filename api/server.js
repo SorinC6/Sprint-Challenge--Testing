efd1c871dcb0d727c2c1d856b6e576358bad453f
Sorin Chis <kish.sorin@yahoo.com>
2019-04-12 10:53:44 +0300

    added first test -GET /games should return status 200

--- api/server.js
+++ api/server.js
@@ -8,4 +8,14 @@ server.get("/", (req, res) => {
   res.json({ message: "Hello from the Server" });
 });
 
+server.get("/games", async (req, res) => {
+  const games = await db("games");
+
+  try {
+    res.status(200).json(games);
+  } catch (error) {
+    res.status(500).json({ error: "error trying to get the games" });
+  }
+});
+
 module.exports = server;
---
+++ api/server.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest");
+const server = require("./server.js");
+
+describe("Testing Endpoints", () => {
+  describe("test GET /games endpoint requests", () => {
+    it("endpoint should return the list of games and HTTP status code 200.", async () => {
+      const response = await request(server).get("/games");
+
+      expect(response.status).toBe(200);
+    });
+  });
+});
